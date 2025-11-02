919f5a730951b5f97e54f384671ae2febdae930c
ramak8sgcp <ramak8sgcp@gmail.com>
2025-11-03 00:46:09 +0530

    added installation points

--- workstation.sh
+++ workstation.sh
@@ -40,6 +40,8 @@ usermod -aG docker ec2-user
 VALIDATE $? "Docker installation"
 
 # eksctl
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
 curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
 tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
 mv /tmp/eksctl /usr/local/bin
